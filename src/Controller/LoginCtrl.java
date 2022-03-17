fcb21c7800b977101963cd54bb36b5d46928c925
acharnovich <charnovich@gmail.com>
2022-03-17 01:47:57 -0400

    test

--- src/Controller/LoginCtrl.java
+++ src/Controller/LoginCtrl.java
@@ -3,24 +3,18 @@ package Controller;
 import Model.StaffList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import javax.imageio.IIOException;
-import java.awt.event.ActionEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
 
-public class LoginCtrl {
-private final StaffList stafflist;
+public class LoginCtrl
+{
+    private final StaffList stafflist;
     @FXML
     private Label idLbl;
 
@@ -40,8 +34,6 @@ private final StaffList stafflist;
     private Label welcomeLbl;
 
 
-
-
     public LoginCtrl()
     {
         stafflist = new StaffList();
@@ -50,37 +42,34 @@ private final StaffList stafflist;
     @FXML
     public void LoginBtnClicked()
     {
-        submitBtn.setOnMouseClicked((event) -> {
-
+        submitBtn.setOnMouseClicked((event) ->
         {
-            // attempt to authenticate user credentials
-            if (stafflist.authenticate(userid.getText(), pin.getText()) == true)
+
             {
+                // attempt to authenticate user credentials
+                if (stafflist.authenticate(userid.getText(), pin.getText()) == true)
+                {
 
-                // if credentials are correct...
-                submitBtn.getScene().getWindow().hide();
-                try{
-               FXMLLoader loader = new FXMLLoader(getClass().getResource("src/View/NavUI.fxml"));
-               loader.setLocation(getClass().getResource("src/View/NavUI.fxml"));
-               Scene scene = new Scene(loader.load(), 600,400);
-               Stage stage = new Stage();
-               stage.setTitle("test");
-               stage.setScene(scene);
-               stage.show();}
-                catch (IOException e){}
+                    // if credentials are correct...
+                    submitBtn.getScene().getWindow().hide();
+                    try
+                    {
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/NavUI.fxml"));
+                        loader.setLocation(getClass().getResource("/View/NavUI.fxml"));
+                        Scene scene = new Scene(loader.load());
+                        Stage stage = new Stage();
+                        stage.setTitle("test");
+                        stage.setScene(scene);
+                        stage.show();
+                    } catch (IOException e)
+                    {}
 
+                }
+            }
 
-
-
-
-               // navCtrl.getNavUI().setVisible(true);       // make Navigation UI visible
-                //loginUI.setVisible(false);                 // make Login UI not visible
-                //loginUI.dispose();                         // get rid of Login UI
-            }}
-
-           // if (staffList.authenticate(loginUI.getUserID().getText(), loginUI.getPin().getText()) == false){
-             //   loginUI.showMessage();
-            });
+            // if (staffList.authenticate(loginUI.getUserID().getText(), loginUI.getPin().getText()) == false){
+            //   loginUI.showMessage();
+        });
         // get the submit button from loginUI and add a mouse listener to it
         //loginUI.getSubmitButton().addMouseListener(new MouseAdapter()
 
