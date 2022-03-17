ad0f7b355c4c8a4b6d278e75de2f915df70b72c7
acharnovich <charnovich@gmail.com>
2022-03-16 22:35:04 -0400

    test

--- src/Controller/LoginCtrl.java
+++ src/Controller/LoginCtrl.java
@@ -1,9 +1,29 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-public class LoginCtrl
-{
+public class LoginCtrl {
 
+    @FXML
+    private Label idLbl;
+
+    @FXML
+    private PasswordField pin;
+
+    @FXML
+    private Label pinLbl;
+
+    @FXML
+    private Button submitBtn;
+
+    @FXML
+    private TextField userid;
+
+    @FXML
+    private Label welcomeLbl;
 
 }
