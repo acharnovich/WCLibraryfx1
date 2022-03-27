f20e6ec9f2e4cf24b00da50e6e446ba8fb508e0c
schuas <100165395+schuas@users.noreply.github.com>
2022-03-27 01:21:48 -0400

    broken new item ui

--- src/Controller/AddOrRemoveItemCtrl.java
+++ src/Controller/AddOrRemoveItemCtrl.java
@@ -1,4 +1,31 @@
 package Controller;
 
-public class AddOrRemoveItemCtrl {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.AnchorPane;
+
+public class AddOrRemoveItemCtrl
+{
+    @FXML
+    private AnchorPane addItemUI;
+
+    @FXML
+    private Label selectTypeLabel;
+
+    @FXML
+    private RadioButton addBookRadioButton;
+
+    @FXML
+    private RadioButton addAudioBookRadioButton;
+
+    @FXML
+    private RadioButton addMovieRadioButton;
+
+    @FXML
+    private Button addItemContinueButton;
+
+
+
 }
--- src/Controller/NavigationCtrl.java
+++ src/Controller/NavigationCtrl.java
@@ -4,14 +4,17 @@ import Model.*;
 import View.FxLoader;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class NavigationCtrl {
@@ -61,44 +64,35 @@ public class NavigationCtrl {
     {
         itemList = new ItemList();
         staffList = new StaffList();
-        importLists();
+        // importLists();
     }
 
     private void importLists() {
-        Book bookTemp;                                 // temp variable for initializing Book objects before adding to inventory
-        Collaborator collabTemp;                       // temp variable for initializing Collaborator objects before adding to collabs
-        ArrayList<Collaborator> collabTempList;        // temp variable for initializing Collaborator ArrayLists before adding to
-                                                       // an Item
-
-        ArrayList<Collaborator> collabs = new ArrayList<>();
-
-        // create a Collaborator object with temp variable
-        collabTemp = new Collaborator("Jason", "Schreier", new ArrayList<Item>(), 1);
-        // make an ArrayList of Collaborators to pass to the Item object
-        collabTempList = new ArrayList<Collaborator>();
-        // add the temp Collaborator object to the collaborators ArrayList
-        collabTempList.add(collabTemp);
-        // add the temp Collaborator to collabs list
-        collabs.add(collabTemp);
-        // create Book object and assign value to temp variable
-        bookTemp = new Book(9101342, "Press Reset", 2021, "May, 11, 2021", "From the bestselling author " +
-                "of Blood, Sweat, and Pixels comes the next definitive, behind-the-scenes account of the video game industry: how "
-                + "some of the past decade's most renowned studios fell apart—and the stories, both triumphant and tragic, of " +
-                "what happened next.", "Checked In", "Grand Central Publishing", collabTempList, "301 " +
-                "pages.", "Non-fiction");
-        // add temp Book to inventory list
-
-        itemList.LoadBook(bookTemp);
     }
 
     public void handleCreateAccount(javafx.event.ActionEvent actionEvent)
-    { newAccountBtn.setOnMouseClicked(mouseEvent -> {
-        FxLoader object = new FxLoader();
-        Pane content = object.getPage("NewAccountUI");
-        navBorder.setLeft(content);
+    {
+        newAccountBtn.setOnMouseClicked(mouseEvent -> {
+            FxLoader object = new FxLoader();
+            Pane content = object.getPage("NewAccountUI");
+            navBorder.setLeft(content);
+        });
+    }
 
-
-
-    });
+    public void handleAddItem(javafx.event.ActionEvent actionEvent)
+    {
+        addItemBtn.setOnMouseClicked(mouseEvent -> {
+            try
+            {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/NewItemUI.fxml"));
+                loader.setLocation(getClass().getResource("/View/NewItemUI.fxml"));
+                Scene scene = new Scene(loader.load());
+                Stage stage = new Stage();
+                stage.setTitle("WCLibrary Add New Item");
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e)
+            {}
+        });
     }
 }
