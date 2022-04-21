c657f3e863ec5d0edc3e4b65d26be8a7c22c1428
schuas <100165395+schuas@users.noreply.github.com>
2022-04-21 00:08:08 -0400

    handling submit click
    doesnt do nothing fancy tho

--- src/Controller/AddBillCtrl.java
+++ src/Controller/AddBillCtrl.java
@@ -1,5 +1,51 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToolBar;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AddBillCtrl
 {
+    @FXML
+    private ToolBar addBillToolBar;
+
+    @FXML
+    private Button addBillSearchButton;
+
+    @FXML
+    private Label addBillLabel;
+
+    @FXML
+    private TextField addBillCardNumTextField;
+
+    @FXML
+    private Button addBillSubmitButton;
+
+    public void handleSubmitClick(javafx.event.ActionEvent actionEvent)
+    {
+        addBillSubmitButton.setOnMouseClicked(mouseEvent -> {
+
+            Parent part = null;
+            try {
+                part = FXMLLoader.load(getClass().getResource("/View/AddBillDetailsUI.fxml"));
+                Stage stage = new Stage();
+                Scene scene = new Scene(part);
+                stage.setScene(scene);
+                stage.setTitle("Enter Bill Details");
+                stage.show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+    }
 }
